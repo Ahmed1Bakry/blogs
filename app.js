08927396f136fd571a2fcf505b79166dd975f36f
Ahmed Bakry <bbakry20@gmail.com>
2021-08-27 23:32:18 +0200

    add Heroku

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 const dbURI = "mongodb+srv://user1:K2BCfSfLOIHKZVf0@cluster0.f18pn.mongodb.net/blogsapp?retryWrites=true&w=majority";
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(3000))
+  .then(result => app.listen(process.env.PORT || 3000))
   .catch(err => console.log(err));
 
 // register view engine
